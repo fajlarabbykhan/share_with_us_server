d9e84118157988d9e7cd572efd2f55122a27785b
Fajla Rabby Khan <fajlarabbyk55@gmail.com>
2022-11-29 10:25:19 +0600

    create user and login user done

--- index.js
+++ index.js
@@ -3,14 +3,18 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
 import morgan from "morgan";
+import userRouter from "./routes/user.js";
 const port = process.env.PORT || 4000;
 const app = express();
 dotenv.config();
 
+//middlewares
 app.use(cors());
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(morgan("dev"));
+
+app.use("/users", userRouter);
 
 mongoose
   .connect(process.env.MONGOURL)
